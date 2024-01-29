ee7506329fc2bdfec948701a643fec432dcf08d4
YEGCSharpDev <89283498+YEGCSharpDev@users.noreply.github.com>
2024-01-28 22:38:55 -0700

    Secondary fix for #1
    Added more logging to catch errors and write to console.

--- LetterboxdToCinephilesChannel/ChannelCalls.cs
+++ LetterboxdToCinephilesChannel/ChannelCalls.cs
@@ -10,33 +10,43 @@ namespace LetterboxdToCinephilesChannel
 
         internal async void SendPhotoAsync(TextMessage parsedData)
         {
-            TelegramBotClient botClient;
-            string Token = Environment.GetEnvironmentVariable("CINEPHILE_TOKEN");
-            string ChatId = Environment.GetEnvironmentVariable("CHAT_ID");
 
-            if (string.IsNullOrEmpty(Token))
+            try
             {
-                Console.WriteLine("Telegram bot token is missing. Set the TELEGRAM_BOT_TOKEN environment variable.");
-                return;
+                TelegramBotClient botClient;
+                string Token = Environment.GetEnvironmentVariable("CINEPHILE_TOKEN");
+                string ChatId = Environment.GetEnvironmentVariable("CHAT_ID");
+
+                if (string.IsNullOrEmpty(Token))
+                {
+                    Console.WriteLine("Telegram bot token is missing. Set the TELEGRAM_BOT_TOKEN environment variable.");
+                    return;
+                }
+                string caption = !string.IsNullOrEmpty(parsedData.MemberRating) ?
+                $"**{EscapeForMarkdown(parsedData.FilmTitle)}\\({EscapeForMarkdown(parsedData.FilmYear)}\\) ** \n{EscapeForMarkdown(parsedData.MemberRating)}\\/{EscapeForMarkdown(parsedData.TotalRating)}\n{EscapeForMarkdown(parsedData.Review)}\n \\- `{EscapeForMarkdown(parsedData.Creator)}`" : $"**{EscapeForMarkdown(parsedData.FilmTitle)}\\({EscapeForMarkdown(parsedData.FilmYear)}\\) **\n{EscapeForMarkdown(parsedData.Review)}\n \\- `{EscapeForMarkdown(parsedData.Creator)}`";
+
+                botClient = new TelegramBotClient(Token);
+
+                ReceiverOptions receiverOptions = new()
+                {
+                    AllowedUpdates = Array.Empty<UpdateType>()
+                };
+
+                using CancellationTokenSource cts = new();
+
+                Message message = await botClient.SendPhotoAsync(
+                chatId: ChatId,
+                photo: InputFile.FromUri(parsedData.ImgSrc),
+                caption: caption,
+                parseMode: ParseMode.MarkdownV2,
+                cancellationToken: cts.Token);
+
             }
-            string caption = !string.IsNullOrEmpty(parsedData.MemberRating) ?
-            $"**{EscapeForMarkdown(parsedData.FilmTitle)}\\({EscapeForMarkdown(parsedData.FilmYear)}\\) ** \n{EscapeForMarkdown(parsedData.MemberRating)}\\/{EscapeForMarkdown(parsedData.TotalRating)}\n{EscapeForMarkdown(parsedData.Review)}\n \\- `{EscapeForMarkdown(parsedData.Creator)}`" : $"**{EscapeForMarkdown(parsedData.FilmTitle)}\\({EscapeForMarkdown(parsedData.FilmYear)}\\) **\n{EscapeForMarkdown(parsedData.Review)}\n \\- `{EscapeForMarkdown(parsedData.Creator)}`";
-
-            botClient = new TelegramBotClient(Token);
-
-            ReceiverOptions receiverOptions = new()
+            catch (Exception ex)
             {
-                AllowedUpdates = Array.Empty<UpdateType>()
-            };
-
-            using CancellationTokenSource cts = new();
-
-            Message message = await botClient.SendPhotoAsync(
-            chatId: ChatId,
-            photo: InputFile.FromUri(parsedData.ImgSrc),
-            caption: caption,
-            parseMode: ParseMode.MarkdownV2,
-            cancellationToken: cts.Token);
+                Console.WriteLine(ex);
+                throw;
+            }
         }
 
         string EscapeForMarkdown(string input)
